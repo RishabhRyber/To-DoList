2b076442d9d1e15711faa95dda7a266625d135f5
namita2310 <namitashetty01@gmail.com>
2020-04-10 22:21:05 +0530

    array created

--- todo.js
+++ todo.js
@@ -1,5 +1,24 @@
+
+let todoItems=[]
+function addTodo(text) {
+    const todo = {
+      text,
+      checked: false,
+      id: Date.now(),
+    };
+  
+    todoItems.push(todo);
+  
+}  
 function add(){
-    var x=document.getElementById("text").value;
-    document.getElementById("list").innerText=x;
+    var x=document.getElementById("todo").value+"\n";
+    todoItems.text=x;
+    todoItems.push(x);
+
+    document.getElementById("list").innerHTML   =todoItems;
 
 }
+
+
+  
+   
