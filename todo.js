428bc053b86329dba6351acc4efd13dfdb9126c7
namita2310 <namitashetty01@gmail.com>
2020-04-12 15:12:59 +0530

    done button updatd

--- todo.js
+++ todo.js
@@ -9,22 +9,45 @@ function addTodo(text) {
   
     todoItems.push(todo);
   
-}  
+} 
+let s;
+var loc;
 function add(){
     var x=document.getElementById("todo").value+"\n";
     addTodo(x);
+    s=s+","+x;
+    localStorage.setItem("task",s)
+    loc=localStorage.getItem("task");
+    console.log(s);
     // todoItems.push(x);
     document.getElementById("list").innerHTML=""
     todoItems.forEach(addNew);
 
 }
-function addNew(value,index){
-  document.getElementById("list").innerHTML  +="<br><hr>"+value['text']+' <input type="checkbox" onclick="del('+index+')" id="check">';
 
+function addNew(value,index){
+    //s=s+","+value['text'];
+  document.getElementById("list").innerHTML  +=`<br><hr>`+value['text']+' <input type="button" value="remove" onclick="del('+index+')" id="check">';
+    loc=localStorage.getItem("task");
+  /* var btn = document.createElement("BUTTON");
+  btn.innerHTML = "CLICK ME";
+  document.body.appendChild(btn);
+  document.body.appendChild(value[text]);*/
 }
 function del(index){
   todoItems.splice(index,1);
+  loc.removeItem(index);
   document.getElementById("list").innerHTML=""
   todoItems.forEach(addNew);
 
+
+}
+
+function submitl(){
+    
+    document.getElementById("list").innerHTML="<h1>This is the list</h1><br>"+loc;
+    s=" ";
+    localStorage.clear();
+    todoItems=[];
+    
 }
